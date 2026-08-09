615ea1db9a02a4adc19fbdbd45f973d5a9386904
runzhiyifengyu <runzhiyifengyu@qq.com>
2026-08-09 21:13:28 +0800

    阶段5(补丁2)

--- src/main/java/dev/cosmonauticsroll/detect/StairBlockQuery.java
+++ src/main/java/dev/cosmonauticsroll/detect/StairBlockQuery.java
@@ -4,6 +4,7 @@ import dev.cosmonauticsroll.api.detect.StairInfo;
 import dev.cosmonauticsroll.api.detect.Vec3d;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockState;
